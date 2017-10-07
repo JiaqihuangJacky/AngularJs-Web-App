502b827dbff8855ce4412d8e21152347b315fe6d
Jacky <huangjiaqi2397056Hjq@gmail.com>
2017-10-07 13:06:46 -0700

    fix some bug

--- src/script/controller/positionCtrl.js
+++ src/script/controller/positionCtrl.js
@@ -1,24 +1,44 @@
 'use strict';
-angular.module('app').controller('positionCtrl', ['$q','$http', '$state', '$scope', function($q,$http, $state, $scope){
-  $scope.isLogin = false;
+angular.module('app').controller('positionCtrl', ['$log', '$q', '$http', '$state', '$scope', 'cache', function($log, $q, $http, $state, $scope, cache){
+  $scope.isLogin = !!cache.get('name');
+  $scope.message = $scope.isLogin?'投个简历':'去登录';
   function getPosition() {
     var def = $q.defer();
-    $http.get('data/position.json?id='+$state.params.id).success(function(resp){
+    $http.get('data/position.json', {
+      params: {
+        id: $state.params.id
+      }
+    }).success(function(resp) {
       $scope.position = resp;
+      if(resp.posted) {
+        $scope.message = '已投递';
+      }
       def.resolve(resp);
-      console.log(resp);
-    }).error(function(err){
+    }).error(function(err) {
       def.reject(err);
     });
-      return def.promise;
+    return def.promise;
   }
-
   function getCompany(id) {
     $http.get('data/company.json?id='+id).success(function(resp){
       $scope.company = resp;
-      })
+    })
   }
-    getPosition().then(function(obj){
-     getCompany(obj.companyId);
-   });
+  getPosition().then(function(obj){
+    getCompany(obj.companyId);
+  });
+  $scope.go = function() {
+    if($scope.message !== '已投递') {
+      if($scope.isLogin) {
+        $http.post('data/handle.json', {
+          id: $scope.position.id
+        }).success(function(resp) {
+          $log.info(resp);
+          $scope.message = '已投递';
+        });
+      } else {
+        $state.go('login');
+      }
+    }
+  }
 }]);
